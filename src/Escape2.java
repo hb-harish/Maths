84b430959c0352261adf5bcab8bc429b24caa6c5
harish <hb.harishbalaji@gmail.com>
2016-09-02 15:19:30 -0400

    Escape 2

---
+++ src/Escape2.java
@@ -0,0 +1,13 @@
+
+public class Escape2 {
+	
+	public static void main(String[] args) {
+		System.out.println("Cost\t      Quantity\tTotal");
+		System.out.println("$1,000.00\t4\t$4,000.00");
+		System.out.println("$   50.00\t8\t$400.00");
+		System.out.println("______________________________");
+		System.out.println("TOTAL:\t\t12\t$4,400.00");
+
+	}
+
+}
