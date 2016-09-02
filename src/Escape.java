f396f89489bac5bfb35703594d3caeebc2b3d791
harish <hb.harishbalaji@gmail.com>
2016-09-02 15:15:12 -0400

    Box

---
+++ src/Escape.java
@@ -0,0 +1,16 @@
+
+public class Escape {
+	
+	public static void main(String[] args) {
+		System.out.println("*********");
+		System.out.println("*\t*");
+		System.out.println("*\t*");
+		System.out.println("*\t*");
+		System.out.println("*\t*");
+		System.out.println("*\t*");
+		System.out.println("*\t*");
+		System.out.println("*********");
+		
+	}
+
+}
