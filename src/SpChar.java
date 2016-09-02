22651d56368a55c03d691e99898ee9e72b0ee7ce
harish <hb.harishbalaji@gmail.com>
2016-09-02 15:27:13 -0400

    Spl Char

---
+++ src/SpChar.java
@@ -0,0 +1,13 @@
+
+public class SpChar {
+	
+	public static void main(String[] args) {
+	    String message1, message2;
+	    
+	    message1 = "message1 = \\/\\/\\/\\/\\/\\r\\t\\b";
+	    message2 = "message2 = \"";
+	    System.out.println( message1 + '\n' + message2);
+	           
+	}
+
+}
